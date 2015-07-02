f4b5da0eb4e56436ec9e777ecb43a17b2edc29f1
Xian Chaobo <xianchaobo@huawei.com>
2015-07-02 17:22:42 +0800

    Create wssh.py

---
+++ wssh.py
@@ -0,0 +1,29 @@
+#!/usr/local/bin/python
+
+import websocket
+import sys
+
+def on_message(ws, message):
+    print message
+def on_error(ws, error):
+    print error
+    exit(2)
+
+def on_close(ws):
+    print "Session Closed"
+
+def on_open(ws):
+    print "Session Open"
+    ws.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print "Need Host!"
+        exit(1)
+    host = sys.argv[1]
+    ws = websocket.WebSocketApp(host,
+                              on_message = on_message,
+                              on_error = on_error,
+                              on_close = on_close)
+    ws.on_open = on_open
+    ws.run_forever()
